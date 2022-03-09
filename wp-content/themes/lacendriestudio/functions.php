6084190be92b788cdbcae986e91b40610970afea
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-03-09 22:37:35 +0100

    Added enqueu scripts & styles

--- wp-content/themes/lacendriestudio/functions.php
+++ wp-content/themes/lacendriestudio/functions.php
@@ -6,6 +6,26 @@
 * @since LaCendrieStudio 1.0
 */
 
+function cendrie_add_theme_scripts() {
+    /* Enqueue styles */
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    /* others stylesheets
+      wp_enqueue_style( 'name_of_file', get_template_directory_uri() . '/assets/css/name_of_file.css', array(), '1.1', 'all' );
+    */
+    
+    /* Enqueue scripts 
+      wp_enqueue_script( 'script', get_template_directory_uri() . '/assets/js/script.js', array ( 'jquery' ), 1.1, true);
+    */
+
+    /* conditional loading script
+      if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+      }
+    */
+    
+}
+add_action( 'wp_enqueue_scripts', 'cendrie_add_theme_scripts' );
+
 /*
 Set maximum content width to 800 pixels
 if ( ! isset( $content_width ) )
