fd3a50e9b5194ca6add7fbb0c19b153281666b87
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-03-16 22:05:51 +0100

    Template-part for header

--- wp-content/themes/lacendriestudio/header.php
+++ wp-content/themes/lacendriestudio/header.php
@@ -11,11 +11,7 @@
 
 <body <?php body_class(''); ?> id="cendrie-body">
   <header>
-    <?php if ( get_header_image() ) : ?>
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-          <img class="mx-auto" src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-        </a>
-    <?php endif; ?>
+    <?php get_template_part( 'template-parts/header/desktop' ); ?>
   </header>
 
   <div class="cendrie-page">
---
+++ wp-content/themes/lacendriestudio/template-parts/header/desktop.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Displays the site header for desktop.
+ *
+ * @package WordPress
+ */
+
+?>
+
+<?php if ( get_header_image() ) : ?>
+  <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+    <img
+      class="mx-auto"
+      src="<?php header_image(); ?>"
+      width="<?php echo absint( get_custom_header()->width ); ?>"
+      height="<?php echo absint( get_custom_header()->height ); ?>"
+      alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+    >
+  </a>
+<?php endif; ?>
