bb23667155ba647bd7d01d6004a9ccfd73babb0d
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-03-20 17:29:11 +0100

    Added carousel into front-home page (before moved in slider plugin)

--- wp-content/themes/lacendriestudio/page-home.php
+++ wp-content/themes/lacendriestudio/page-home.php
@@ -22,4 +22,38 @@ if ( have_posts() ) :
 else :
   _e( 'Désolé, nous n\'avons pas trouvé de résultats.' );
 endif;
+
+
+$args = array(
+    'post_type'      => 'slider',
+    'posts_per_page' => 3,
+);
+$loop = new WP_Query($args);
+?>
+<div class="text-base md:text-xl lg:text-2xl px-4 md:container mx-auto">
+  <div class="sm:w-full md:w-11/12 lg:w-9/12 mx-auto text-justify leading-relaxed">
+    <div id="carouselExampleSlidesOnly" class="carousel slide relative" data-bs-ride="carousel">
+      <div class="carousel-inner relative w-full overflow-hidden">
+        <?php
+          $counter = 0;
+          while ( $loop->have_posts() ) {
+            $loop->the_post();
+            $class_ac = '';
+            if($counter === 0){
+              $class_ac = 'active';
+            }
+          ?>
+            <div class="carousel-item relative float-left h-[20rem] md:h-[30rem] lg:h-[35rem] xl:h-[40rem] w-full <?php echo $class_ac; ?>">
+              <?php the_post_thumbnail('large', ['class' => 'block h-[20rem] md:h-[30rem] lg:h-[35rem] xl:h-[40rem] mx-auto']); ?>
+            </div>
+          <?php
+            $counter++;
+          }
+          wp_reset_query();
+        ?>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
 get_footer(); ?>
