fe33db0b8aebc28cf13be002eb39f8765802f83b
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-04-07 11:37:02 +0200

    Fixed bad resizing of images

--- wp-content/themes/lacendriestudio/page-home.php
+++ wp-content/themes/lacendriestudio/page-home.php
@@ -8,18 +8,17 @@
 if (!defined('ABSPATH')) { exit; }?>
 
 <?php get_header();
-
-if ( have_posts() ) : 
-  while ( have_posts() ) : the_post();
+  if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
     ?>
       <div class="text-base md:text-xl lg:text-2xl px-4 md:container mx-auto home-content">
         <div class="sm:w-full md:w-11/12 lg:w-9/12 mx-auto text-justify leading-relaxed">
-          <?php the_content(); ?>
+            <?php the_content(); ?>
         </div>
       </div>
     <?php
-  endwhile;
-else :
-  _e( 'Désolé, nous n\'avons pas trouvé de résultats.' );
+    endwhile;
+  else :
+    _e( 'Désolé, nous n\'avons pas trouvé de résultats.' );
 endif;
 get_footer();
