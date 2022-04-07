d07efeed7565a9f0e4b2df0894c214d195b57342
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-04-07 14:49:59 +0200

    Centered & stylised footer text

--- wp-content/themes/lacendriestudio/footer.php
+++ wp-content/themes/lacendriestudio/footer.php
@@ -2,7 +2,15 @@
 
   </div><!-- Closing of cendrie-page -->
 
-  <footer></footer>
+  <footer id="cendrie-footer">
+    <div class="p-4 md:container mx-auto home-content">
+      <div class="sm:w-full md:w-11/12 lg:w-9/12 mx-auto text-center leading-relaxed text-white">
+        <p class="text-base md:text-lg lg:text-xl">
+          <a href="<?php echo esc_url( home_url( '/nous-contacter' ) ); ?>">Nous contacter</a>
+        </p>
+      </div>
+    </div>
+  </footer>
 
   <?php wp_footer(); ?>
 
