d7b36cd10c0dbd496afe11dbf775a2f9d396d927
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-04-07 14:34:55 +0200

    Remove header's home link if current page is front

--- wp-content/themes/lacendriestudio/template-parts/header/desktop.php
+++ wp-content/themes/lacendriestudio/template-parts/header/desktop.php
@@ -7,14 +7,24 @@
 
 ?>
 
-<?php if ( get_header_image() ) : ?>
-  <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-    <img
-      class="mx-auto"
-      src="<?php header_image(); ?>"
-      width="<?php echo absint( get_custom_header()->width ); ?>"
-      height="<?php echo absint( get_custom_header()->height ); ?>"
-      alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
-    >
-  </a>
+<?php if ( get_header_image() ) :
+  if ( is_front_page() ) : ?>
+  <img
+    class="mx-auto"
+    src="<?php header_image(); ?>"
+    width="<?php echo absint( get_custom_header()->width ); ?>"
+    height="<?php echo absint( get_custom_header()->height ); ?>"
+    alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+  >
+  <?php else : ?>
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+      <img
+        class="mx-auto"
+        src="<?php header_image(); ?>"
+        width="<?php echo absint( get_custom_header()->width ); ?>"
+        height="<?php echo absint( get_custom_header()->height ); ?>"
+        alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+      >
+    </a>
+	<?php endif; ?>
 <?php endif; ?>
