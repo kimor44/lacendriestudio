1e48aec2b5c202774a3c26dc27c02c12fa76636d
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-04-07 12:04:21 +0200

    Adatped text & marges sizing according to the screen size

--- wp-content/themes/lacendriestudio/page.php
+++ wp-content/themes/lacendriestudio/page.php
@@ -3,11 +3,11 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : 
   ?>
-    <div class="text-base md:text-xl lg:text-2xl px-4 md:container mx-auto home-content">
+    <div class="text-lg md:text-xl lg:text-2xl px-4 md:container mx-auto home-content">
       <div class="sm:w-full md:w-11/12 lg:w-9/12 mx-auto text-justify leading-relaxed text-white">
       <?php
         while ( have_posts() ) : the_post();
-          the_title('<h2 class="text-lg">', '</h2>');
+          the_title('<h1 class="text-3xl font-semibold">', '</h1>');
           the_content();
         endwhile;
       ?>
