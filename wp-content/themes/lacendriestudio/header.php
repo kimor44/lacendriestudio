22370549f04cfecca1dcbfd467704f45a7d9b64c
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-04-14 17:46:29 +0200

    Footer sticky at the bottom of the page

--- wp-content/themes/lacendriestudio/header.php
+++ wp-content/themes/lacendriestudio/header.php
@@ -1,7 +1,7 @@
 <?php if (!defined('ABSPATH')) { exit; }?>
 
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="cendrie-h-full">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="<?php bloginfo( 'charset' ); ?>"/>
 <?php wp_head(); ?> <!-- Load WP objects for head-tag -->
@@ -9,7 +9,7 @@
 </head>
 
 
-<body <?php body_class(''); ?> id="cendrie-body">
+<body <?php body_class('cendrie-body-template cendrie-h-full'); ?> id="cendrie-body">
   <header>
     <?php get_template_part( 'template-parts/header/desktop' ); ?>
   </header>
