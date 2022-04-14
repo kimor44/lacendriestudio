ee634390af9a30b3c654be4c50728d79e3daaed9
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-04-14 17:46:29 +0200

    Footer sticky at the bottom of the page

--- wp-content/themes/lacendriestudio/footer.php
+++ wp-content/themes/lacendriestudio/footer.php
@@ -2,7 +2,7 @@
 
   </div><!-- Closing of cendrie-page -->
 
-  <footer id="cendrie-footer">
+  <footer id="cendrie-footer" class="shrink">
     <div class="p-4 md:container mx-auto home-content">
       <div class="sm:w-full md:w-11/12 lg:w-9/12 mx-auto text-center leading-relaxed text-white">
         <p class="text-base md:text-lg lg:text-xl">
