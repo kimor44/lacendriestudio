be5477e0d05a8a294870617767440b61c469718a
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-04-24 23:08:54 +0200

    Add control arrows in the carousel

--- wp-content/plugins/tw-sliders/tw-sliders.php
+++ wp-content/plugins/tw-sliders/tw-sliders.php
@@ -97,7 +97,7 @@ function tw_sliders_init(){
 
     $sliders = new WP_Query($args); 
 
-    $carousel .= '<div id="carouselExampleSlidesOnly" class="carousel slide relative my-4 md:my-5 lg:my-6 xl:my-8 2xl:my-10" data-bs-ride="carousel">';
+    $carousel .= '<div id="carouselExampleControls" class="carousel slide relative" data-bs-ride="carousel">';
     $carousel .= '<div class="carousel-inner relative w-full overflow-hidden">';
 
     while ( $sliders->have_posts() ) {
@@ -113,7 +113,27 @@ function tw_sliders_init(){
       $carousel .= '</div></div>';
     }
     wp_reset_query();
-    $carousel .= '</div></div>';
+    $carousel .= '</div>
+    <button
+      class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+      type="button"
+      data-bs-target="#carouselExampleControls"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+      type="button"
+      data-bs-target="#carouselExampleControls"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>';
+
+    $carousel .= '</div>';
     return $carousel;
   }
 }
