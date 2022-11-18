a39a32f3cbb77f2c6ec48c6ecacb9c89166e2393
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-11-18 22:34:13 +0100

    feat(API REST): Require authentication for API REST wp-json
    Closes #72

--- wp-content/themes/lacendriestudio/functions.php
+++ wp-content/themes/lacendriestudio/functions.php
@@ -7,6 +7,11 @@
 */
 
 /*
+* Require authentication for API REST
+*/
+require_once('includes/reset-wp-json.php');
+
+/*
 * Remove useless image sizes
 */
 function cendrie_remove_extra_image_sizes() {
