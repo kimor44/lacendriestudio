59bd9bf5eb7a7eb0cf9924983b22bd7081378c2c
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2022-11-18 23:37:37 +0100

    feat(is_visible meta_box): Added NONCE fields
    & added verification about capability
    
    Closes #73

--- wp-content/plugins/tw-sliders/class-carousel-metabox.php
+++ wp-content/plugins/tw-sliders/class-carousel-metabox.php
@@ -1,20 +1,25 @@
 <?php
 
 abstract class Carousel_Metabox {
+
+  const META_KEY = 'is_visible';
+  const NONCE = '_cendrie_is_visible_nonce';
  
   /**
    * Create the checkbox metabox in the admin panel of "sliders" plugin
    * to show or not the slide in the carousel
   */
-  public static function add_checkbox_is_visible() {
-    add_meta_box(
-        'is_visible',
-        'Afficher l\'image dans le carrousel ?',
-        [ self::class, 'build_is_visible_form' ],
-        'slider',
-        'advanced',
-        'high',
-    );
+  public static function add_checkbox_is_visible($postType, $post) {
+    if($postType == 'slider' && current_user_can('publish_posts', $post)){
+      add_meta_box(
+          self::META_KEY,
+          'Afficher l\'image dans le carrousel ?',
+          [ self::class, 'build_is_visible_form' ],
+          'slider',
+          'advanced',
+          'high',
+      );
+    }
   }
 
   /**
@@ -23,14 +28,20 @@ abstract class Carousel_Metabox {
    * @param int $post_id The ID of the post being saved.
    */
   public static function save_is_visible_postdata( $post_id ) {
-    if(!array_key_exists('is_visible', $_POST)){
-      $_POST['is_visible'] = "no";
+    $nonce = $_POST[self::NONCE];
+    if (
+        wp_verify_nonce($nonce, self::NONCE) &&
+        current_user_can('publish_posts', $post_id)
+        ) {
+        if(!array_key_exists(self::META_KEY, $_POST)){
+          $_POST[self::META_KEY] = "no";
+        }
+        update_post_meta(
+            $post_id,
+            'is_visible_meta_key',
+            $_POST[self::META_KEY],
+        );
     }
-    update_post_meta(
-        $post_id,
-        'is_visible_meta_key',
-        $_POST['is_visible'],
-    );
   }
 
   /**
@@ -41,12 +52,14 @@ abstract class Carousel_Metabox {
   public static function build_is_visible_form( $post ) {
     $value = get_post_meta( $post->ID, 'is_visible_meta_key', true );
     $checked = $value == "yes" ? "checked" : "";
+		// Add an nonce field so we can check for it later.
+		wp_nonce_field( self::NONCE, self::NONCE );
     ?>
-      <input type="checkbox" id="is_visible" name="is_visible" value="yes" <?php echo $checked; ?>>
-      <label for="is_visible">Cocher la case pour afficher l'image dans le carrousel</label>
+      <input type="checkbox" id="<?= self::META_KEY ?>" name="<?= self::META_KEY ?>" value="yes" <?php echo $checked; ?>>
+      <label for="<?= self::META_KEY ?>">Cocher la case pour afficher l'image dans le carrousel</label>
     <?php
   }
 }
 
-add_action( 'add_meta_boxes', [ 'Carousel_Metabox', 'add_checkbox_is_visible' ] );
+add_action( 'add_meta_boxes', [ 'Carousel_Metabox', 'add_checkbox_is_visible' ], 10, 2 );
 add_action( 'save_post', [ 'Carousel_Metabox', 'save_is_visible_postdata' ] );
