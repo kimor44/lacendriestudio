3f1070d2388dfcb98ba57b07003b4e7c2ea31b67
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2023-09-29 16:34:50 +0200

    fix(Updated MU plugin filter_end_time_slot): + updated gitignore

--- wp-content/mu-plugins/filter_end_time_slot.php
+++ wp-content/mu-plugins/filter_end_time_slot.php
@@ -1,72 +1,75 @@
 <?php
-/**
- * Plugin Name: Filter end time slot
+
+/*
+ * Plugin Name:       Filter end time slot
  * Description: Change end time slot for edge case.
- * Author:      Julien Guibert
- * Version1: 25 03 2022
+ * Version:           2.0.0
+ * Requires PHP:      7.2
+ * Author:            Julien Guibert
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 // Basic security, prevents file from being loaded directly.
-defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+defined('ABSPATH') or die('Cheatin&#8217; uh?');
 
-/**
- * Pass your custom function to the wp_rocket_loaded action hook.
- *
- * Note: wp_rocket_loaded itself is hooked into WordPress’ own
- * plugins_loaded hook.
- * Depending what kind of functionality your custom plugin
- * should implement, you can/should hook your function(s) into
- * different action hooks, such as for example
- * init, after_setup_theme, or template_redirect.
- * 
- * Learn more about WordPress actions and filters here:
- * https://developer.wordpress.org/plugins/hooks/
- */
-// add_action( 'wp_rocket_loaded', 'yourprefix__do_something' );
+// Number of seconds in one hour constant
+define("SECONDS_IN_HOUR", 60 * 60);
 
 /**
  * catch and set the end time when timeslot is 10:00 or 14:00
- * 
+ *
  * @return array Array of timeslots
  */
- function cendrie_filter_timeslots( $timeslots, $day, $service_id )
+function cendrie_set_end_timeslots($timeslots, $day, $service_id)
 {
-	
-	foreach($timeslots as $timeslot){
-		$formated_timeslot = $timeslot->get_formated_time();
-		if($formated_timeslot == '10:00 - 13:00'){
-			$timeslot->set($timeslot->getStart(), $timeslot->getEnd() + 3600);
-			$timeslot->set_formated_time('10:00 - 14:00');
-			$timeslot->set_formated_time_backend('10:00 - 14:00');
-		} elseif ($formated_timeslot == '14:00 - 17:00'){
-			$timeslot->set($timeslot->getStart(), $timeslot->getEnd() + 3600);
-			$timeslot->set_formated_time('14:00 - 18:00');
-			$timeslot->set_formated_time_backend('14:00 - 18:00');
+	$disallawed_hours = ['13', '17'];
+	foreach ($timeslots as $timeslot) {
+		$end = $timeslot->get_end();
+		$end_hour = date('H', $end);
+		if (in_array($end_hour, $disallawed_hours)) {
+			// Set start & end of the timeslot
+			$end_timeslot = $end + SECONDS_IN_HOUR;
+			$timeslot->set($timeslot->get_start(), $end_timeslot);
+
+			// Set the formated time of the timeslot
+			$hour_plus_one = (string) ((int) $end_hour + 1);
+			$formated_time = str_replace($end_hour, $hour_plus_one, $timeslot->get_formated_time());
+			$timeslot->set_formated_time($formated_time);
+
+			// Set the formated time local of the timeslot
+			$local_hour_plus_one = (string) ((int) $hour_plus_one + 1);
+			$formated_time_local = str_replace($hour_plus_one, $local_hour_plus_one, $timeslot->get_formated_time_local());
+			$timeslot->set_formated_time_local($formated_time_local);
+
+			// Set the formated time backend of the timeslot
+			$formated_time_backend = str_replace($end_hour, $hour_plus_one, $timeslot->get_formated_time_backend());
+			$timeslot->set_formated_time_backend($formated_time_backend);
 		}
 	}
-
 	return $timeslots;
 }
-add_filter( 'get_time_slots_by_day', 'cendrie_filter_timeslots', 10, 3 );
+add_filter('get_time_slots_by_day', 'cendrie_set_end_timeslots', 10, 3);
 
-function cendrie_set_correct_end_timeslot($form_html, $service_id, $first_time){
-	// doing development...	
-	$patterns = array();
-	$patterns[0] = '/13/';
-	$patterns[1] = '/17/';
-	$pat = '/13/';
-	// $patterns[0] = '/10:00 - 13:00/';
-	// $patterns[1] = '/14:00 - 17:00/';
+// function cendrie_set_correct_end_timeslot($form_html, $service_id, $first_time)
+// {
+// 	do_action('inspect', ['form html', $form_html]);
+// 	// doing development...
+// 	$patterns = array();
+// 	$patterns[0] = '/13/';
+// 	$patterns[1] = '/17/';
+// 	$pat = '/13/';
+// 	// $patterns[0] = '/10:00 - 13:00/';
+// 	// $patterns[1] = '/14:00 - 17:00/';
 
-	$replacements = array();
-	$replacements[0] = '14';
-	$replacements[1] = '18';
-	$rep = '14';
+// 	$replacements = array();
+// 	$replacements[0] = '14';
+// 	$replacements[1] = '18';
+// 	$rep = '14';
 
-	$form_html = preg_replace($pat, $rep, $form_html);
-	// $form_html = preg_replace($patterns, $replacements, $form_html);
+// 	$form_html = preg_replace($pat, $rep, $form_html);
+// 	// $form_html = preg_replace($patterns, $replacements, $form_html);
 
-	return $form_html;
-}
-add_filter('wbk_form_html', 'cendrie_set_correct_end_timeslot', 10, 3);
-
+// 	return $form_html;
+// }
+// add_filter('wbk_form_html', 'cendrie_set_correct_end_timeslot', 10, 3);
