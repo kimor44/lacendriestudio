c2b64ccc2ddf10d78c075ec66d998c3c47588249
Julien Guibert <54883200+kimor44@users.noreply.github.com>
2023-10-11 09:45:10 +0200

    feat(filter_end_time_slot): Appply discount if the whole morning is booked

--- wp-content/mu-plugins/filter_end_time_slot.php
+++ wp-content/mu-plugins/filter_end_time_slot.php
@@ -16,12 +16,20 @@ defined('ABSPATH') or die('Cheatin&#8217; uh?');
 // Number of seconds in one hour constant
 define("SECONDS_IN_HOUR", 60 * 60);
 
+// Start hours of discount
+define("CONSECUTIVE_HOURS_FOR_DISCOUNTS", ['10', '14']);
+
+// Applied price for the timeslot when 2 consecutive bookings in the same morning
+define("DISCOUNT_OF_5_EUROS", 5);
+
 /**
  * catch and set the end time when timeslot is 10:00 or 14:00
+ * Retrieve original file :
+ * wp-content/plugins/webba-booking-lite/includes/processors/class-wbk-schedule-processor.php
  *
  * @return array Array of timeslots
  */
-function cendrie_set_end_timeslots($timeslots, $day, $service_id)
+function cendrie_set_end_timeslots($timeslots, $day, $service_id): array
 {
 	$disallawed_hours = ['13', '17'];
 	foreach ($timeslots as $timeslot) {
@@ -64,31 +72,92 @@ function cendrie_set_end_timeslots($timeslots, $day, $service_id)
 			 */
 			$formated_time_backend = str_replace($end_hour, $end_hour_plus_one_hour, $timeslot->get_formated_time_backend());
 			$timeslot->set_formated_time_backend($formated_time_backend);
+
+			/**
+			 * set_offset
+			 */
+			$timeslot->set_offset(0);
 		}
 	}
 	return $timeslots;
 }
 add_filter('get_time_slots_by_day', 'cendrie_set_end_timeslots', 10, 3);
 
-// function cendrie_set_correct_end_timeslot($form_html, $service_id, $first_time)
-// {
-// 	do_action('inspect', ['form html', $form_html]);
-// 	// doing development...
-// 	$patterns = array();
-// 	$patterns[0] = '/13/';
-// 	$patterns[1] = '/17/';
-// 	$pat = '/13/';
-// 	// $patterns[0] = '/10:00 - 13:00/';
-// 	// $patterns[1] = '/14:00 - 17:00/';
+/**
+ * Check if the morning of the current booking is booked
+ *
+ * @param string $start number of start timeslot
+ * @param int $day timestamp of the current booking's day
+ * @param WBK_Booking[] $timeslots array of timeslots
+ * @return bool true if th morning of the current booking is booked
+ */
+function is_morning_booked(string $start, int $day, array $timeslots): bool
+{
+	foreach ($timeslots as $slot) {
+		$slot_start = $slot->get_start();
+		if ($slot->get_day() == $day && $slot_start != $start) {
+			if (in_array(date('H', $slot_start), CONSECUTIVE_HOURS_FOR_DISCOUNTS)) {
+				return true;
+			}
+		}
+	}
 
-// 	$replacements = array();
-// 	$replacements[0] = '14';
-// 	$replacements[1] = '18';
-// 	$rep = '14';
+	return false;
+}
 
-// 	$form_html = preg_replace($pat, $rep, $form_html);
-// 	// $form_html = preg_replace($patterns, $replacements, $form_html);
+/**
+ * Apply discount if the morning of booking is booked
+ * Retrieve origingal file :
+ * wp-content/plugins/webba-booking-lite/includes/processors/class-wbk-price-processor.php
+ *
+ * @param string $default_price Price applied before discount
+ * @param WBK_Booking $booking Data of the current booking
+ * @param WBK_Booking[] $bookings array of bookings
+ * @return string the new price applied
+ */
+function cendrie_princing_for_booked_morning(string $default_price, WBK_Booking $booking, array $bookings): string
+{
+	$cloned_default_price = $default_price;
+	$start = $booking->get_start();
+	$start_hour = date('H', $start);
 
-// 	return $form_html;
-// }
-// add_filter('wbk_form_html', 'cendrie_set_correct_end_timeslot', 10, 3);
+	if (in_array($start_hour, CONSECUTIVE_HOURS_FOR_DISCOUNTS)) {
+		$is_morning_booked = is_morning_booked($start, $booking->get_day(), $bookings);
+		if ($is_morning_booked) {
+			$cloned_default_price = (string) (intval($cloned_default_price) - DISCOUNT_OF_5_EUROS);
+		}
+	}
+
+	return $cloned_default_price;
+}
+add_filter('webba_after_pricing_rule_applied', 'cendrie_princing_for_booked_morning', 10, 3);
+
+/**
+ * Retrieve original files :
+ * wp-content/plugins/webba-booking-lite/deprecated/class_wbk_service_schedule.php
+ * wp-content/plugins/webba-booking-lite/templates/backend/schedule_day_timeslot.php
+ * wp-content/plugins/webba-booking-lite/templates/backend/schedule_day_timeslots.php
+ */
+function cendrie_time_control($time_controls, $timeslot, $service_id)
+{
+	if ($timeslot->offset !== 0) {
+		$offset = $timeslot->offset;
+		$timeslot->set(($timeslot->start - $offset), ($timeslot->end - $offset));
+		$timeslot->set_formated_time_local($timeslot->formated_time);
+		$timeslot->set_offset(0);
+	}
+
+	return $time_controls;
+}
+add_filter('wbk_backend_schedule_time_controls', 'cendrie_time_control', 10, 3);
+
+/**
+ * Retrieve original file :
+ * wp-content/plugins/webba-booking-lite/includes/class-wbk-request-manager.php
+ */
+function cendrie_form_validation($wbk_external_validation, $POST)
+{
+	$POST['offset'] = '0';
+	return $wbk_external_validation;
+}
+add_filter('wbk_booking_form_validation', 'cendrie_form_validation', 10, 2);
